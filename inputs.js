c51421c04bda61f8f5eb83b80bbfc85c9b193e5a
Grzegorz Dubiel <greg2012201@gmail.com>
2021-09-29 13:46:51 +0200

    Create the inputs function.

---
+++ inputs.js
@@ -0,0 +1,33 @@
+const inputs = function (getElementsToAppend) {
+  let options = [];
+  let nodesNumber = 0;
+
+  const getData = function (data) {
+    return transformData(data);
+  };
+  const transformData = function (data) {
+    nodesNumber = data.length;
+    data.forEach(({miejscowosc, ulica}) => {
+      createElement(miejscowosc, 'city');
+      if (!ulica) return;
+      createElement(ulica, 'street');
+    });
+  };
+
+  const createElement = function (text, name) {
+    const fragment = $(document.createDocumentFragment());
+    const option = $(
+      `<option value="${text.toLowerCase()}" name="${name}" ">${text}</option>`
+    );
+
+    $(fragment).append(option);
+    options.push(fragment);
+    console.log(options);
+    if (options.length === nodesNumber) {
+      getElementsToAppend(options);
+      return (options = []);
+    }
+  };
+
+  return {getData};
+};
