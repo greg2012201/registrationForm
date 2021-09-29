d745a4c981fba360c1db128d1fa553bfbd507521
Grzegorz Dubiel <greg2012201@gmail.com>
2021-09-29 13:48:52 +0200

    Call the cleanUp conditionaly in the handleKeyUp

--- handleKeyUp.js
+++ handleKeyUp.js
@@ -1,7 +1,10 @@
-const handleKeyUp = function (event, query) {
+const handleKeyUp = function (event, query, cleanUp) {
   const inputMaxLength = parseInt($(event.target).attr('maxlength'), 10);
   const inputCurrLength = event.target.value.length;
   const inputValue = event.target.value;
+  if (inputCurrLength === 0) {
+    cleanUp();
+  }
   if (inputCurrLength === inputMaxLength) {
     return query(inputValue);
   }
