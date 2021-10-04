16d180129d9806f385fa6f3d106b052fa378c8eb
Grzegorz Dubiel <greg2012201@gmail.com>
2021-10-04 10:34:16 +0200

    Remove cleanUp call.

--- handleKeyUp.js
+++ handleKeyUp.js
@@ -2,9 +2,7 @@ const handleKeyUp = function (event, query, cleanUp) {
   const inputMaxLength = parseInt($(event.target).attr('maxlength'), 10);
   const inputCurrLength = event.target.value.length;
   const inputValue = event.target.value;
-  if (inputCurrLength === 0) {
-    cleanUp();
-  }
+
   if (inputCurrLength <= 1) {
   }
   if (inputCurrLength === inputMaxLength) {
